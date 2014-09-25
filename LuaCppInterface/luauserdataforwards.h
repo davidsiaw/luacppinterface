b25532a26e3defdf74434cb430e6d50b279af002
David Siaw <davidsiaw@gmail.com>
2014-09-25 14:02:05 +0900

    make bind possible for const-functions too

--- LuaCppInterface/luauserdataforwards.h
+++ LuaCppInterface/luauserdataforwards.h
@@ -52,3 +52,58 @@
 
 	template<typename RET, typename ARG0, typename ARG1, typename ARG2, typename ARG3, typename ARG4, typename ARG5, typename ARG6, typename ARG7>
 	void BindYield(std::string name, RET(TYPE::*func)(ARG0, ARG1, ARG2, ARG3, ARG4, ARG5, ARG6, ARG7) );
+
+	template<typename RET>
+	void Bind(std::string name, RET(TYPE::*func)() const );
+
+	template<typename RET, typename ARG0>
+	void Bind(std::string name, RET(TYPE::*func)(ARG0) const );
+
+	template<typename RET, typename ARG0, typename ARG1>
+	void Bind(std::string name, RET(TYPE::*func)(ARG0, ARG1) const );
+	
+	template<typename RET, typename ARG0, typename ARG1, typename ARG2>
+	void Bind(std::string name, RET(TYPE::*func)(ARG0, ARG1, ARG2) const );
+
+	template<typename RET, typename ARG0, typename ARG1, typename ARG2, typename ARG3>
+	void Bind(std::string name, RET(TYPE::*func)(ARG0, ARG1, ARG2, ARG3) const );
+
+	template<typename RET, typename ARG0, typename ARG1, typename ARG2, typename ARG3, typename ARG4>
+	void Bind(std::string name, RET(TYPE::*func)(ARG0, ARG1, ARG2, ARG3, ARG4) const );
+
+	template<typename RET, typename ARG0, typename ARG1, typename ARG2, typename ARG3, typename ARG4, typename ARG5>
+	void Bind(std::string name, RET(TYPE::*func)(ARG0, ARG1, ARG2, ARG3, ARG4, ARG5) const );
+
+	template<typename RET, typename ARG0, typename ARG1, typename ARG2, typename ARG3, typename ARG4, typename ARG5, typename ARG6>
+	void Bind(std::string name, RET(TYPE::*func)(ARG0, ARG1, ARG2, ARG3, ARG4, ARG5, ARG6) const );
+
+	template<typename RET, typename ARG0, typename ARG1, typename ARG2, typename ARG3, typename ARG4, typename ARG5, typename ARG6, typename ARG7>
+	void Bind(std::string name, RET(TYPE::*func)(ARG0, ARG1, ARG2, ARG3, ARG4, ARG5, ARG6, ARG7) const );
+	
+	template<typename RET>
+	void BindYield(std::string name, RET(TYPE::*func)() const );
+
+	template<typename RET, typename ARG0>
+	void BindYield(std::string name, RET(TYPE::*func)(ARG0) const );
+
+	template<typename RET, typename ARG0, typename ARG1>
+	void BindYield(std::string name, RET(TYPE::*func)(ARG0, ARG1) const );
+	
+	template<typename RET, typename ARG0, typename ARG1, typename ARG2>
+	void BindYield(std::string name, RET(TYPE::*func)(ARG0, ARG1, ARG2) const );
+
+	template<typename RET, typename ARG0, typename ARG1, typename ARG2, typename ARG3>
+	void BindYield(std::string name, RET(TYPE::*func)(ARG0, ARG1, ARG2, ARG3) const );
+
+	template<typename RET, typename ARG0, typename ARG1, typename ARG2, typename ARG3, typename ARG4>
+	void BindYield(std::string name, RET(TYPE::*func)(ARG0, ARG1, ARG2, ARG3, ARG4) const );
+
+	template<typename RET, typename ARG0, typename ARG1, typename ARG2, typename ARG3, typename ARG4, typename ARG5>
+	void BindYield(std::string name, RET(TYPE::*func)(ARG0, ARG1, ARG2, ARG3, ARG4, ARG5) const );
+
+	template<typename RET, typename ARG0, typename ARG1, typename ARG2, typename ARG3, typename ARG4, typename ARG5, typename ARG6>
+	void BindYield(std::string name, RET(TYPE::*func)(ARG0, ARG1, ARG2, ARG3, ARG4, ARG5, ARG6) const );
+
+	template<typename RET, typename ARG0, typename ARG1, typename ARG2, typename ARG3, typename ARG4, typename ARG5, typename ARG6, typename ARG7>
+	void BindYield(std::string name, RET(TYPE::*func)(ARG0, ARG1, ARG2, ARG3, ARG4, ARG5, ARG6, ARG7) const );
+	
