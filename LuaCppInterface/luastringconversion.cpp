fc4b9441ebbfae444de33b1475f12e8ae70052ae
David Siaw <davidsiaw@gmail.com>
2016-03-24 00:48:04 +0900

    remove iconv. use c++11 codecvt for non-windows build

--- LuaCppInterface/luastringconversion.cpp
+++ LuaCppInterface/luastringconversion.cpp
@@ -57,66 +57,22 @@ std::string WStrToUTF8(const std::wstring& wstr)
 }
 
 #else
-#include <iconv.h>
-#include <errno.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <codecvt>
+#include <locale>
 std::wstring UTF8ToWStr(const std::string& str)
 {
-	size_t c_wide_len = str.size();
-	size_t c_wide_bytes = c_wide_len * sizeof(wchar_t);
+	std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>,wchar_t> convert;
+	std::wstring dest = convert.from_bytes(str);
 
-	size_t c_mb_len = str.size() + 1;
-	size_t c_mb_bytes = c_mb_len * sizeof(char);
-
-	wchar_t* c_wide = new wchar_t[c_wide_len];
-	char* c_mb = new char[c_mb_len];
-
-	memset(c_wide, 0, c_wide_bytes);
-	memset(c_mb, 0, c_mb_bytes);
-	memcpy(c_mb, str.c_str(), str.size());
-
-	char* m_wide = (char*)c_wide;
-	char* m_mb = c_mb;
-
-	iconv_t cd = iconv_open("UTF-16LE", "UTF-8");
-	size_t inbytesleft = c_mb_bytes;
-	size_t outbytesleft = c_wide_bytes;
-	size_t res = iconv(cd, &m_mb, &inbytesleft, &m_wide, &outbytesleft);
-
-	std::wstring wide_result(c_wide);
-	delete[] c_wide;
-	delete[] c_mb;
-	iconv_close(cd);
-
-	return wide_result;
+	return dest;
 }
 
 std::string WStrToUTF8(const std::wstring& str)
 {
-	size_t max_size = str.size() * 3;
-	wchar_t* c_wide = new wchar_t[str.size()+1];
-	char* c_mb = new char[max_size];
-	memset(c_wide, 0, (str.size()+1) * sizeof(wchar_t));
-	memset(c_mb, 0, max_size * sizeof(char));
-	memcpy(c_wide, str.c_str(), str.size() * sizeof(wchar_t));
+	std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>,wchar_t> convert;
+	std::string dest = convert.to_bytes(str);  
 
-	char* m_wide = (char*)c_wide;
-	char* m_mb = c_mb;
-
-	iconv_t cd = iconv_open("UTF-8", "UTF-16LE");
-	size_t inbytesleft = str.size() * sizeof(wchar_t);
-	size_t outbytesleft = max_size * sizeof(char);
-	size_t res = iconv(cd, &m_wide, &inbytesleft, &m_mb, &outbytesleft);
-	//std::cout << "in:" << inbytesleft << " orig:" << str.size() * sizeof(wchar_t) << std::endl;
-
-	std::string result(c_mb);
-	delete[] c_wide;
-	delete[] c_mb;
-	iconv_close(cd);
-
-	return result;
+	return dest;
 }
 #endif
 
