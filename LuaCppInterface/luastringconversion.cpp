179e8c74f3824afdafec9c49f0c58f64f069e013
David Siaw <davidsiaw@gmail.com>
2016-05-18 17:15:54 +0900

    test run a real test. fix up windows utf8 utf16 conversion

--- LuaCppInterface/luastringconversion.cpp
+++ LuaCppInterface/luastringconversion.cpp
@@ -4,41 +4,87 @@
 #include <cstdlib>
 #include <iostream>
 
+#ifdef _MSC_VER
+#include <windows.h>
+#endif
+
 std::wstring UTF8ToWStr(const std::string& utf8str)
 {
+#ifdef _MSC_VER
+	// Because windows does not accept UTF-8 as a locale, we have to use its
+	// other functions that do
+	std::wstring wstr;
+	int wlen = MultiByteToWideChar(CP_UTF8, NULL, utf8str.c_str(), utf8str.size(), NULL, NULL);
+	
+	if (wlen == 0)
+	{
+		return L"String conversion error";
+	}
+
+	wstr.resize(wlen);
+
+	wlen = MultiByteToWideChar(CP_UTF8, NULL, utf8str.c_str(), utf8str.size(), (wchar_t*)wstr.data(), wlen);
+	
+	if (wlen == 0)
+	{
+		return L"String conversion error";
+	}
+
+	return wstr;
+#else
 	auto original_locale = setlocale(LC_ALL, NULL);
 	auto loc = setlocale(LC_ALL, "en_US.UTF-8");
-	wchar_t* wstr_buf = new wchar_t[utf8str.size()];
-#ifdef _MSC_VER
-	size_t converted;
-	mbstowcs_s(&converted, wstr_buf, utf8str.size(), utf8str.c_str(), utf8str.size());
-#else
+	if (loc == NULL)
+	{
+		return L"String conversion error: locale en_US.UTF-8 not supported";
+	}
+	size_t wstr_buf_size = utf8str.size() + 1;  // +1 because there is a null-terminator
+	wchar_t* wstr_buf = new wchar_t[wstr_buf_size];
 	size_t converted = mbstowcs(wstr_buf, utf8str.c_str(), utf8str.size());
-#endif
 	std::wstring result(wstr_buf);
 	delete[] wstr_buf;
 	setlocale(LC_ALL, original_locale);
 
 	return result;
+#endif
 }
 
 #define MAX_UTF8_CHAR_CHAIN 5
 
 std::string WStrToUTF8(const std::wstring& wstr)
 {
+#ifdef _MSC_VER
+	std::string utf8str;
+	int len = WideCharToMultiByte(CP_UTF8, NULL, wstr.c_str(), wstr.size(), NULL, NULL, NULL, NULL);
+	
+	if (len == 0)
+	{
+		return "String conversion error";
+	}
+
+	utf8str.resize(len);
+	len = WideCharToMultiByte(CP_UTF8, NULL, wstr.c_str(), wstr.size(), (char*)utf8str.data(), len, NULL, NULL);
+	
+	if (len == 0)
+	{
+		return "String conversion error";
+	}
+
+	return utf8str;
+#else
 	auto original_locale = setlocale(LC_ALL, NULL);
 	auto loc = setlocale(LC_ALL, "en_US.UTF-8");
-	size_t max_size = wstr.size() * MAX_UTF8_CHAR_CHAIN;
+	if (loc == NULL)
+	{
+		return "String conversion error: locale en_US.UTF-8 not supported";
+	}
+	size_t max_size = wstr.size() * MAX_UTF8_CHAR_CHAIN + 1;   // +1 for the null terminator
 	char* str_buf = new char[max_size];
-#ifdef _MSC_VER
-	size_t converted;
-	wcstombs_s(&converted, str_buf, max_size, wstr.c_str(), max_size);
-#else
 	size_t converted = wcstombs(str_buf, wstr.c_str(), max_size);
-#endif 
 	std::string result(str_buf);
 	delete[] str_buf;
 	setlocale(LC_ALL, original_locale);
 
 	return result;
+#endif 
 }
--- LuaCppInterfaceTests/unittest1.cpp
+++ LuaCppInterfaceTests/unittest1.cpp
@@ -1,17 +1,45 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
 
+#include <iostream>
+#include <memory>
+#include <luacppinterface.h>
+
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace LuaCppInterfaceTests
 {		
-	TEST_CLASS(UnitTest1)
+	TEST_CLASS(UnitTests)
 	{
 	public:
 		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(testchartowideconvert)
 		{
-			// TODO: Your test code here
+			std::string utf8str("ABC\xCE\x99\xCE\xBF\xCF\x8D\xCE\xBD\xCE\xB9\xCE\xBA\xCE\xBF\xCE\xBD\xCF\x84\x00");
+			std::wstring wstr(L"ABC\u0399\u03BF\u03CD\u03BD\u03B9\u03BA\u03BF\u03BD\u03C4");
+
+			auto conv = UTF8ToWStr(utf8str);
+			if (conv == wstr)
+			{
+				return Assert::IsFalse(0);
+			}
+
+			std::cout << "Expect: ";
+			for (int i=0;i<wstr.size();i++)
+			{
+				std::cout << (unsigned int)wstr[i] << " ";
+			}
+			std::cout << " " << wstr.size();
+			std::cout << std::endl;
+
+			std::cout << "Result: ";
+			for (int i=0;i<conv.size();i++)
+			{
+				std::cout << (unsigned int)conv[i] << " ";
+			}
+			std::cout << " " << conv.size();
+			std::cout << std::endl;
+			return Assert::IsFalse(1);
 		}
 
 	};
